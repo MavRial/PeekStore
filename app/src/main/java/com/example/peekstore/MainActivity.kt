43c48bced5b89e13e3ea80a445344ced31351f9c
maverick <maverickgshepherd@gmail.com>
2025-05-21 23:14:07 -0300

    fire base setup y coneccion con login

--- app/src/main/java/com/example/peekstore/MainActivity.kt
+++ app/src/main/java/com/example/peekstore/MainActivity.kt
@@ -9,13 +9,8 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.LocalContext
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.example.peekstore.data.service.TokenManager
-import com.example.peekstore.presentation.home.HomeScreen
 import com.example.peekstore.presentation.login.LoginScreen
 import com.example.peekstore.presentation.login.viewmodel.LoginViewModel
 
